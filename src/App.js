e80a7cbf5e848d1ab9a6a2c11bcc8f04fae0ce31
RodrigoN <rdg6design@gmail.com>
2022-06-17 09:46:48 -0300

    Listening to User input

--- src/App.js
+++ src/App.js
@@ -29,8 +29,6 @@ const App = () => {
 ]
   return (
     <div>
-        <h2> Let-s get started!</h2>
-        <p>This is also visible!</p>
         <NewExpense />
         <Expenses items={expenses}/>
     </div>
