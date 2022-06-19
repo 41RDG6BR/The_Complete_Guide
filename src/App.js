2626e25a6736653e2ca4e2655b92f4e39275dc40
RodrigoN <rdg6design@gmail.com>
2022-06-19 05:30:31 -0300

    Working with Lists && Outputting Conditional Content

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ const DUMMY_EXPENSES = [{
   id: 'e1',
   title: 'Toilet Paper',
   amount: 494.67,
-  date: new Date(2021, 2, 28)
+  date: new Date(2019, 2, 28)
   },
   {
     id: 'e2',
     title: 'New TV',
     amount: 294.67,
-    date: new Date(2021, 2, 28)
+    date: new Date(2020, 2, 28)
   },
   {
     id: 'e3',
     title: 'Car Insurance',
     amount: 375.87,
-    date: new Date(2021, 2, 28)
+    date: new Date(2019, 2, 28)
   },
   {
     id: 'e4',
